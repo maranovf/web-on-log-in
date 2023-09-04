05fb3995f1d93e5de6fd62a706bc9fd5a9fc7a16
Marek Novotný <141149691+maranovf@users.noreply.github.com>
2023-09-04 15:32:22 +0200

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,13 +1,13 @@
 <?php
-$server = 'localhost'; /* Název serveru, ke kterému se budeme připojovat */
-$name = 'root'; /* Jméno uživatele do DB */
-$password = 'root'; /* Heslo uživatele do DB */
-$db_name = 'users'; /* Název databáze, ve které jsme si vytvořili tabulku "uzivatele" */
+$server = 'localhost'; /* Address of the database server */
+$name = 'root'; /* Database username */
+$password = 'root'; /* Database password */
+$db_name = 'users'; /* Name of database */
 
 $db = mysqli_connect($server, $name, $password, $db_name);
 
 if (!$db) {
-    die("Připojení selhalo: " . mysqli_connect_error());
+    die("Database connection error: " . mysqli_connect_error());
     }
 
 echo "";
