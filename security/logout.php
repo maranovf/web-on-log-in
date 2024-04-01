3697aa145120a67f2740fb5488143e5d74734446
Marek Novotný <maranovf@seznam.cz>
2024-04-01 19:34:10 +0200

    Bug fix

--- security/logout.php
+++ security/logout.php
@@ -1,7 +1,11 @@
 <?php
-   session_start();
+    include ("../security/session.php");
+
+    session_start();
+
+    session_unset();
    
-   if(session_destroy()) {
-      header("Location: ../Public/");
+    if(session_destroy()) {
+        header("Location: ../Public/");
    }
 ?>
