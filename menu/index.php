906a4dd11f53f5d73acbddef23b4b9208dbebfed
Marek Novotný <maranovf@seznam.cz>
2024-08-25 00:07:06 +0200

    Added search bar.

--- menu/index.php
+++ menu/index.php
@@ -16,6 +16,7 @@
         <li><a href="../files">Files</a></li>
         <li><a href="../upload/">File upload</a></li>
         <li><a href="../management_redirect/">Management</a></li>
+        <li><form method="post" action=""><table><tr><td><input type="search" id="search" name="search" placeholder="Hledat" style="margin: 2% 0%; padding: 2% 2%;"></td><td><input type="button" id="search_button" value="Vyhledat" style="margin: 2% 2%; padding: 2% 2%;"></td></tr></table></form></li>
         <li style="float:right"><a href = "../security/logout.php">Sign out</a></li>
     </ul>
 
